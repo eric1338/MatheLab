0dd13faed01212f031f834647d30e27e7613f0e9
Eric <snehvid@gmx.de>
2016-12-13 14:37:52 +0100

    Aufgabe 3.6 fertig und Aufgabe 4.8 weitergemacht

--- Blatt02/nonLinRoots.py
+++ Blatt02/nonLinRoots.py
@@ -1,7 +1,22 @@
 print("roots [Ex. 4.8 a-f)]")
 
 import math;
+import time;
 import matplotlib.pyplot as plt;
+
+
+def fFixed(x):
+	return 0.5 * pow(math.e, -pow(x, 2));
+
+
+def f(x):
+	return fFixed(x) - x;
+
+
+def fDerived(x):
+	return -pow(math.e, -pow(x, 2)) * x;
+
+
 
 print("a)");
 
@@ -13,7 +28,8 @@ for i in range(200):
 	x = (i / 100.0) - 1;
 	
 	xses.append(x);
-	fLeft.append(0.5 * pow(math.e, -pow(x, 2)));
+	fLeft.append(fFixed(x));
+	#fLeft.append(0.5 * pow(math.e, -pow(x, 2)));
 	fRight.append(x);
 
 
@@ -21,5 +37,91 @@ plt.plot(xses, fLeft);
 plt.plot(xses, fRight);
 plt.xlabel('x');
 plt.ylabel('y');
-plt.show();
+#plt.show();
 
+
+
+	
+
+def mySign(x):
+	return -1 if x < 0 else 1;
+
+
+
+def getRootBisectionRec(xa, xb, maxSteps, stepsTaken):
+	xm = xa + (xb - xa) / 2;
+	
+	if stepsTaken >= maxSteps: return xm;
+	
+	newXA = xa;
+	newXB = xb;
+	
+	if mySign(f(xa)) != mySign(f(xm)):
+		newXB = xm;
+	else:
+		newXA = xm;
+
+	return getRootBisectionRec(newXA, newXB, maxSteps, stepsTaken + 1);
+
+
+def getRootBisection(xa, xb, maxSteps):
+	if mySign(f(xa)) == mySign(f(xb)):
+		print("fail");
+		return -1;
+	
+	return getRootBisectionRec(xa, xb, maxSteps, 0);
+
+
+
+def getRootNewtonRec(x, maxSteps, stepsTaken):
+	fD = fDerived(x);
+	
+	if abs(fD) < 0.00001: fD = 0.00001;
+	
+	nextX = x - (f(x) / fD);
+	
+	if stepsTaken < maxSteps:
+		return getRootNewtonRec(nextX, maxSteps, stepsTaken + 1)
+	else:
+		return nextX;
+
+
+def getRootNewton(x0, maxSteps):
+	return getRootNewtonRec(x0, maxSteps, 0);
+
+
+
+def getFixedPointRec(x, maxSteps, stepsTaken):
+	newX = fFixed(x);
+	
+	if stepsTaken < maxSteps:
+		return getFixedPointRec(newX, maxSteps, stepsTaken + 1);
+	else:
+		return newX;
+
+
+def getFixedPoint(x0, maxSteps):
+	return getFixedPointRec(x0, maxSteps, 0);
+
+
+
+def getTime():
+	return time.time();
+
+def printTimeDiff(startTime, endTime):
+	diff = endTime - startTime;
+	print("diff: " + str(diff));
+
+
+print(" ");
+print("b)");
+
+rootBisection = getRootBisection(0.01, 1, 10);
+rootNewton = getRootNewton(0.5, 10);
+
+print(rootBisection);
+print(rootNewton);
+
+fixedPoint = getFixedPoint(0, 10);
+
+print(fixedPoint);
--- Blatt02/randomNumbers.py
+++ Blatt02/randomNumbers.py
@@ -1,16 +1,17 @@
-print("random [Ex. 3.6 a-e)]")
+print("random [Ex. 3.6 a-d)]")
 
 import math;
 
 
 class RNG:
-	a = 0;
-	b = 0;
-	m = 0;
-	x = 0;
-	bits = [];
+	#a = 0;
+	#b = 0;
+	#m = 0;
+	#x = 0;
+	#bits = [];
 	
 	def __init__(self, pA = 7141, pB = 54773, pM = 259200):
+		self.bits = [];
 		self.x = 1;
 		self.a = pA;
 		self.b = pB;
@@ -50,6 +51,55 @@ class RNG:
 		print("deviation = " + str(deviation));
 	
 	
+	def isPeriod(self, periodLength):
+		nBits = len(self.bits);
+		
+		if periodLength * 2 > nBits: return False;
+		
+		iterations = int(math.floor(nBits / float(periodLength)));
+		
+		for i in range(periodLength):
+			compareElement = self.bits[i];
+		
+			for j in range(1, iterations):
+				key = j * periodLength + i;
+				
+				if key >= nBits: break;
+				
+				element = self.bits[key];
+				
+				if element != compareElement:
+					return False;
+		
+		return True;
+	
+	
+	def makePeriodicityTest(self):
+		periodLength = -1;
+		
+		maximumPeriod = self.m * int(math.ceil(math.log(self.m, 2)));
+		
+		if maximumPeriod > 100002: maximumPeriod = 100002;
+		
+		for i in range(1, maximumPeriod):
+			if i > 100000:
+				periodLength = -2;
+				break;
+			
+			if self.isPeriod(i):
+				periodLength = i;
+				break;
+		
+		print("periodicty test:");
+		
+		if periodLength < -1.5:
+			print("no period shorter than 100,000 found.");
+		elif periodLength < -0.5:
+			print("no period found.");
+		else:
+			print("period length = " + str(periodLength));
+
+	
 	def applyNeumannFilter(self):
 		tempBits = self.bits;
 		self.bits = [];
@@ -71,26 +121,45 @@ class RNG:
 print("b)");
 
 rng = RNG();
-rng.fillNumbers(1000);
+rng.fillNumbers(25000);
 rng.makeSymmetryTest();
+print(" ");
+rng.makePeriodicityTest();
+
 
 print(" ");
 print("c)");
+rng.fillNumbers(75000);
 rng.applyNeumannFilter();
 rng.makeSymmetryTest();
+print(" ");
+rng.makePeriodicityTest();
 
 print(" ");
 print("d)");
 
-rng1 = RNG(100, 1000, 10000);
+rng1 = RNG(1, 1, 10);
 rng1.printParameters();
 
-rng1.fillNumbers(1000);
+rng1.fillNumbers(25000);
+print(len(rng1.bits));
 rng1.makeSymmetryTest();
 
-rng2 = RNG(1000, 10000, 100000);
+
+print(" ");
+
+rng2 = RNG(20, 30, 100000);
 rng2.printParameters();
 
-rng2.fillNumbers(1000);
+rng2.fillNumbers(25000);
 rng2.makeSymmetryTest();
+
+print(" ");
+
+rng3 = RNG(214013, 2531011, pow(2, 32));
+rng3.printParameters();
+
+rng3.fillNumbers(25000);
+rng3.makeSymmetryTest();
+
 
